72c1e39d988149c5221219c9d31863c1060621f7
Juan José Angarita Yela <129020936+RamonaFlores@users.noreply.github.com>
2023-10-02 13:04:20 -0500

    Project was created

---
+++ demo/src/main/java/model/Task.java
@@ -0,0 +1,13 @@
+package model;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Task {
+   private int identifier;
+   private String title;
+   private String description;
+   private Calendar deadline;
+   private Boolean isReminder;
+   public Task(String title,String )
+}
