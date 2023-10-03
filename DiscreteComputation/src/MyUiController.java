847b425c989ef81458e6957e503fc84dff691bff
Juan José Angarita Yela <129020936+RamonaFlores@users.noreply.github.com>
2023-10-03 10:06:12 -0500

    User interface was created

---
+++ DiscreteComputation/src/MyUiController.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MyUiController extends JFrame {
+    private JPanel panel1;
+    private JComboBox comboBox1;
+    private JButton undoButton;
+    private JButton leaveButton;
+    private JLabel titleImage;
+
+    public MyUiController() {
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    private void createUIComponents() {
+
+    }
+}
---
+++ DiscreteComputation/src/ui/Main.java
@@ -0,0 +1,10 @@
+package ui;
+
+import view.MyUiController;
+
+public class Main {
+    MyUiController ui;
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
