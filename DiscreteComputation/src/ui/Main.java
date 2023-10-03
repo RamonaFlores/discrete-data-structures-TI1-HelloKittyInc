479c5061c7018192aad694a156c5f2881d050a54
Juan José Angarita Yela <129020936+RamonaFlores@users.noreply.github.com>
2023-10-03 15:46:45 -0500

    functional menu

--- DiscreteComputation/src/ui/Main.java
+++ DiscreteComputation/src/ui/Main.java
@@ -1,10 +1,9 @@
 package ui;
 
-import view.MyUiController;
 
 public class Main {
-    MyUiController ui;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
 }
---
+++ DiscreteComputation/src/ui/MainScreenUI.java
@@ -0,0 +1,39 @@
+package ui;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MainScreenUI extends JFrame {
+
+    private JPanel MainPanel;
+    private JLabel Bear;
+    private JLabel Logo;
+    private JLabel ghibli;
+    private JButton UndoButton;
+    private JButton ExitButton;
+    private JComboBox comboBox1;
+    private JLabel message;
+
+    public MainScreenUI(){
+        setContentPane(MainPanel);
+        setTitle("Remind");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(590,630);
+        setLocationRelativeTo(null);
+        setVisible(true);
+        comboBox1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int option=comboBox1.getSelectedIndex();
+                switch(option){
+                    case 0:
+                }
+            }
+        });
+    }
+    public static void main(String[] args){
+        new MainScreenUI();
+    }
+
+}
