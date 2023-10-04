956d81d7d7790544de61feec51c34aabbf52ba95
PollitoHDK <raul.quigua2@gmail.com>
2023-10-04 12:10:12 -0500

    Clonable interface implemented

--- DiscreteComputation/src/model/MyController.java
+++ DiscreteComputation/src/model/MyController.java
@@ -2,13 +2,27 @@ package model;
 
 import util.HashTableChaining;
 
-public class MyController  {
+import java.util.Stack;
+
+public class MyController implements Cloneable {
     private HashTableChaining hashTableChaining;
+    private Stack<MyController> versionController;
     public MyController() {
         this.hashTableChaining=new HashTableChaining<>();
+        this.versionController = new Stack<>();
 
     }
     public void addTask(String title,String description,String deadline,boolean isReminder,int priority){
+
+    }
+
+    @Override
+    public void saveState() {
+
+    }
+
+    @Override
+    public void restoreState() {
 
     }
 }
