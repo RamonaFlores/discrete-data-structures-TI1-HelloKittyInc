fae578ac6fb4418dab371eb2273b7b406caef977
PollitoHDK <raul.quigua2@gmail.com>
2023-10-05 12:36:12 -0500

    Hashtable Implementation is now completed YAHOOO, there where some fights between my work group that made me kinda sad but i feel like it's part of life to realize that maybe it's okay to be not okay sometimes for real haha

--- DiscreteComputation/src/ui/DeleteScreen.java
+++ DiscreteComputation/src/ui/DeleteScreen.java
@@ -1,10 +1,37 @@
 package ui;
 
-import javax.swing.*;
+import model.MyController;
 
-public class DeleteScreen {
-    private JTextField textField1;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class DeleteScreen extends JFrame{
+    private JTextField ans;
     private JLabel image;
     private JLabel textField;
-    private JButton button1;
+    private JButton removeButton;
+    private JPanel delete;
+
+    public DeleteScreen(MyController controller){
+        setContentPane(delete);
+        setTitle("Task delete");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500,500);
+        setLocationRelativeTo(null);
+        setVisible(false);
+        removeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                String idstr=ans.getText();
+                int identifier=Integer.parseInt(idstr);
+                controller.deleteTask(identifier);
+
+                setVisible(false);
+
+            }
+        });
+    }
+
 }
